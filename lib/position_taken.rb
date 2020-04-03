d1b07969302929aeab701750e6d00f1e4c768ee2
Micah White <micahjwhite@gmail.com>
2020-04-03 01:29:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    taken = false
+    return false
   elsif board[index] == "X" || board[index] == "O"
-    taken = true
+    return true
   end
 end
